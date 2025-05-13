252a04dcc3a874d75a43a79ca1ab1d3aa9de698a
Nalaka Manathunga <nalaka.manathunga@gmail.com>
2025-05-13 16:45:50 +0200

    feat(validations:backend): add schema validations for pagination and auth

---
+++ backend/src/validations/common.validations.ts
@@ -0,0 +1,14 @@
+import { paginationConfig } from '@/config/app.config'
+import { z } from 'zod'
+
+export const genericPaginationSchema = z.object({
+  page: z.number().min(1).default(1),
+  limit: z
+    .number()
+    .min(1)
+    .max(paginationConfig.MaxLimit)
+    .default(paginationConfig.DefaultLimit),
+  offset: z.number().min(0).default(0),
+  sort: z.string().default('createdAt'),
+  order: z.enum(['asc', 'desc']).default('desc'),
+})
