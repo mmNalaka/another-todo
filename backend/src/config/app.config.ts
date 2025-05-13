aa0c37fb52603d94bf7ecd885cb32c997218d276
Nalaka Manathunga <nalaka.manathunga@gmail.com>
2025-05-13 17:03:04 +0200

    refactor(config:backend): add pagination config and use const assertion

--- backend/src/config/app.config.ts
+++ backend/src/config/app.config.ts
@@ -1,4 +1,9 @@
 export const appConfig = {
   name: 'Ubiquity TODOs REST API',
   version: '0.0.1',
-}
+} as const
+
+export const paginationConfig = {
+  DefaultLimit: 10,
+  MaxLimit: 100,
+} as const
