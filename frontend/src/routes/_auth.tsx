f576bb6acc597ac19748ab1e1859ea87aaadebc1
Nalaka Manathunga <nalaka.manathunga@gmail.com>
2025-05-22 23:04:03 +0200

    refactor(auth-layout): simplify header structure and add user greeting

--- frontend/src/routes/_auth.tsx
+++ frontend/src/routes/_auth.tsx
@@ -2,18 +2,13 @@ import { Separator } from '@radix-ui/react-separator'
 import { Outlet, createFileRoute, redirect } from '@tanstack/react-router'
 import { AppSidebar } from '@/components/sidebar/app-sidebar'
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb'
-import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from '@/components/ui/sidebar'
+import { useLocalization } from '@/hooks/use-localization'
+import { useAuth } from '@/providers/auth-provider'
+import { getCurrentDate } from '@/lib/utils'
 
 export const Route = createFileRoute('/_auth')({
   beforeLoad: ({ context, location }) => {
@@ -30,28 +25,26 @@ export const Route = createFileRoute('/_auth')({
 })
 
 function AuthenticatedLayout() {
+  const { t } = useLocalization()
+  const { user } = useAuth()
+
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
+        <header className="flex shrink-0 items-center gap-2">
+          <div className="flex items-center justify-between p-2 border-b w-full">
+            <SidebarTrigger className="m-2" />
             <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">Todo Application</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>My Tasks</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+            <div>
+              <h2 className="text-xl font-semibold">
+                {t('tasks.app.greeting')}, {user && user.name} 👋
+              </h2>
+              <p className="text-gray-500 text-sm">{getCurrentDate()}</p>
+            </div>
           </div>
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <main className="flex flex-1 flex-col gap-4 pt-0">
           <Outlet />
         </main>
       </SidebarInset>
