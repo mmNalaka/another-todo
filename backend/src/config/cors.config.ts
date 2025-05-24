498ad14a29ed14d14a7a5ca768d361e2d34abec2
Nalaka Manathunga <nalaka.manathunga@gmail.com>
2025-05-24 12:32:24 +0200

    fix(cors): add 'PATCH' to allowed methods in CORS config

--- backend/src/config/cors.config.ts
+++ backend/src/config/cors.config.ts
@@ -5,5 +5,5 @@ type CORSOptions = Parameters<typeof cors>[0]
 export const corsOptions: CORSOptions = {
   origin: '*',
   allowHeaders: ['*'],
-  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
 }
